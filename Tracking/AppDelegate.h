a858ce24b5927e883d97aadcfdd37f1b43f0197b
付州 <fu363128432@qq.com>
2016-04-14 10:03:35 +0800

    Initial Commit

---
+++ Tracking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tracking
+//
+//  Created by 付州  on 16/4/14.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tracking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tracking
+//
+//  Created by 付州  on 16/4/14.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
